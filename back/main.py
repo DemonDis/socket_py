c32ef04f59b2afbb29158e7397f1ac3b5a73cf61
DemonDis <50297797+DemonDis@users.noreply.github.com>
2024-03-25 22:44:21 +0700

    🚧 .env

--- back/main.py
+++ back/main.py
@@ -1,27 +1,30 @@
-import socket
+import os, socket
+from dotenv import load_dotenv
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 3031 # Port to listen on (non-privileged ports are > 1023)
+load_dotenv('./.env')
 
-def start_server():
-    # Создаем сокет
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((HOST, PORT))
+HOST = os.getenv("HOST")
+PORT = os.getenv("PORT")
 
-    # Слушаем входящие соединения
-    server.listen(1)
-    print("Сервер запущен и ожидает подключений...")
+# def start_server():
+# Создаем сокет
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
 
-    # Принимаем входящее соединение
-    client_socket, client_address = server.accept()
-    print(f"Подключение установлено с {client_address}")
-    
-    # Получаем данные от клиента, по 1024 байт
-    data = client_socket.recv(1024).decode('utf-8')
-    print(f"Получены данные: {data}")
-    HDRS = 'HTTP/1.1 200 OK\r\nContent-Type text/html; charset=utf-8\r\n\r\n'
+# Слушаем входящие соединения
+server.listen(1)
+print("Сервер запущен и ожидает подключений...")
 
-    content = 'Good'.encode('utf-8')
+# Принимаем входящее соединение
+client_socket, client_address = server.accept()
+print(f"Подключение установлено с {client_address}")
 
-    client_socket.send(HDRS.encode('utf-8') + content)
-    print('Shutdown')
+# Получаем данные от клиента, по 1024 байт
+data = client_socket.recv(1024).decode('utf-8')
+print(f"Получены данные: {data}")
+HDRS = 'HTTP/1.1 200 OK\r\nContent-Type text/html; charset=utf-8\r\n\r\n'
+
+content = 'Good'.encode('utf-8')
+
+client_socket.send(HDRS.encode('utf-8') + content)
+print('Shutdown')
